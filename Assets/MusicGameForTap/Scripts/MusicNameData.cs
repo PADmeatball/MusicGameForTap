b309491eeb60ebb177c8f440f77aa5102c64dd9b
Padmeatball <padmeatball299@gmail.com>
2018-10-07 18:35:56 +0900

    Add MusicNameScript. This Script is Save MusicName.

---
+++ Assets/MusicGameForTap/Scripts/MusicNameData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicNameData : MonoBehaviour {
+
+    public const string BGM_ShinigStar = "ShinigStar";
+
+}
--- Assets/MusicGameForTap/Scripts/StartMusic.cs
+++ Assets/MusicGameForTap/Scripts/StartMusic.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class StartMusic : MonoBehaviour {
 
+    //流したい曲を入れる
     AudioSource targetMusic;
+    //押した後に非表示にしたいためButtonUIをいれる。
     public GameObject MusicStaratButton;
+
  
 	// Use this for initialization
 	void Start () {
 
         targetMusic = GetComponent<AudioSource>();
-
+        
     }
 
     // Update is called once per frame
