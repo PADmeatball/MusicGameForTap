2afdb033a049b152e811ca13c19d555449e9fabe
Padmeatball <padmeatball299@gmail.com>
2018-10-07 20:31:26 +0900

    デモ動画用に入力を可視化するScript ChangeColorを追加

---
+++ Assets/MusicGameForTap/Scripts/MoveNoteToCreateScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveNoteToCreateScene : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(0, 0, speed * Time.deltaTime);
+	}
+}
