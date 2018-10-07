97e7e4ee86b54a95f29d9fada931726ca25e3744
Padmeatball <padmeatball299@gmail.com>
2018-10-07 17:10:55 +0900

    Add StartMusicButton,StartMusicScript and Music(ShiningStar)

---
+++ Assets/MusicGameForTap/Music/MusicDetail.cs
@@ -0,0 +1,8 @@
+﻿/*
+ MusicData　　提供元
+
+ ShinigStar  魔王魂
+  
+  
+  
+*/
---
+++ Assets/StartMusic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMusic : MonoBehaviour {
+
+    AudioSource targetMusic;
+    public GameObject MusicStaratButton;
+ 
+	// Use this for initialization
+	void Start () {
+
+        targetMusic = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+    public void OnPushedBottun()
+    {
+        targetMusic.Play();
+        MusicStaratButton.SetActive(false);
+    }
+}
