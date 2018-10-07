397e5740f9f7390a7a453754679ed185d594d916
Padmeatball <padmeatball299@gmail.com>
2018-10-07 20:07:42 +0900

    入力された指に応じて音符が生成されるScript GenerateNotesを追加

---
+++ Assets/MusicGameForTap/Scripts/NotesGenerate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesGenerate : MonoBehaviour {
+
+    InputManager fingerInput;
+    //Notesを生成するポイントを入れるための配列
+    public GameObject[] generateNotesPoint;
+
+    //生成したい音符を入れる
+    [SerializeField] GameObject Notes;
+
+	// Use this for initialization
+	void Start () {
+        
+        fingerInput = GameObject.Find("InputManager").GetComponent<InputManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        NotesCreate();
+
+    }
+    void NotesCreate()
+    {
+        //どの指で入力されたのかを確認
+        for (int fingerNum = 0; fingerNum <= 4; fingerNum++)
+        {
+            if (fingerInput.whatFinger[fingerNum])
+            {
+                Destroy(Instantiate(Notes, generateNotesPoint[fingerNum].transform.position, Quaternion.identity), 3);
+                
+                fingerInput.whatFinger[fingerNum] = false;
+            }
+            
+        }
+    }
+   
+}
